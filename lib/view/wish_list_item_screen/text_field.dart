16f0414e337bb3169ca9a49ea00c9a83f4b6ac7f
tkana03 <yangtanglujiu@gmail.com>
2024-07-14 06:34:16 +0900

    top他変更

--- lib/view/wish_list_item_screen/text_field.dart
+++ lib/view/wish_list_item_screen/text_field.dart
@@ -2,38 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class ItemNameField extends HookWidget {
-  const ItemNameField({super.key, required this.controller});
+  const ItemNameField(
+      {super.key, required this.controller, required this.focusNode});
   final TextEditingController controller;
+  final FocusNode focusNode;
 
   @override
   Widget build(BuildContext context) {
-    final isFocused = useState(false);
-
     return GestureDetector(
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      behavior: HitTestBehavior.opaque,
+      // onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      onTap: () => FocusScope.of(context).unfocus(),
+      behavior: HitTestBehavior.translucent,
       child: SizedBox(
         width: 300,
-        child: Focus(
-          onFocusChange: (hasFocus) {
-            isFocused.value = hasFocus;
-          },
-          child: TextFormField(
-            controller: controller,
-            obscureText: false,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              labelText: '商品',
-              prefixIcon: const Icon(Icons.shop),
-              suffixIcon: isFocused.value ? const Icon(Icons.clear) : null,
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return '商品名を入力してください';
-              }
-              return null;
-            },
+        child: TextFormField(
+          focusNode: focusNode,
+          controller: controller,
+          obscureText: false,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: '商品',
+            prefixIcon: Icon(Icons.shop),
           ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '商品名を入力してください';
+            }
+            return null;
+          },
+          onTapOutside: (e) => focusNode.unfocus(),
         ),
       ),
     );
